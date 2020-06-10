b5d01f2389aa1d6ea17235bd423b81418e4b0188
Tasja <tasjalaurie@outlook.com>
2020-06-10 23:19:35 +1000

    add topic to resources

--- fan-out/handlers/create.js
+++ fan-out/handlers/create.js
@@ -1,39 +1,67 @@
-'use strict';
+"use strict";
 
-const uuid = require('uuid');
-const AWS = require('aws-sdk');
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const sns = new AWS.SNS();
+
+const { TOPIC_ARN } = process.env;
 
 module.exports.handler = (event, context, callback) => {
-
   const data = JSON.parse(event.body);
 
-  const params = {
+  const tableParams = {
     TableName: process.env.USER_TABLE,
     Item: {
       id: uuid.v1(),
       fullName: data.fullName,
       email: data.email,
-      phone: data.phone
+      phone: data.phone,
     },
   };
 
-  dynamoDb.put(params, (error) => {
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the user.',
-      });
-      return;
-    }
+  try {
+    dynamoDb.put(tableParams, (error) => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { "Content-Type": "text/plain" },
+          body: "Couldn't create the user.",
+        });
+        return;
+      }
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(tableParams.Item),
+      };
+      callback(null, response);
+    });
+
+    const topicParams = {
+      Message: tableParams.Item,
+      TopicArn: TOPIC_ARN,
     };
-    callback(null, response);
-  });
+
+    sns.publish(topicParams, (error) => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: 501,
+          headers: { "Content-Type": "text/plain" },
+          body: "Couldn't send message.",
+        });
+      }
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Successfully sent message" }),
+      };
+      callback(null, response);
+    });
+  } catch (e) {
+    console.log("Error: ", e);
+  }
 };
